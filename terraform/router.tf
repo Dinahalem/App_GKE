fa3157ef504a44edf41fb69ab11cc1b4df975715
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2024-05-02 23:24:24 +0300

    Update router.tf

--- terraform/router.tf
+++ terraform/router.tf
@@ -1,6 +1,5 @@
 #create Cloud Router to advertise routes. It will be used with the NAT gateway to allow VMs without public IP addresses to access the internet
 
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_router
 resource "google_compute_router" "router" {
   name    = "router"
   region  = "us-central1"
