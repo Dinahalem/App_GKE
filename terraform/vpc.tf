4c8c6da2476010ab142dbcebffbf177316bdce84
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2024-05-03 00:52:42 +0300

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,14 +1,17 @@
 #enable compute API
 resource "google_project_service" "compute" {
+  project = "devops-v4-e2"
   service = "compute.googleapis.com"
 }
 
 resource "google_project_service" "container" {
+  project = "devops-v4-e2"
   service = "container.googleapis.com"
 }
 
 
 resource "google_compute_network" "main" {
+  project                         = "devops-v4-e2"
   name                            = "main"
   routing_mode                    = "REGIONAL"
   auto_create_subnetworks         = false
