46d00d9d26ac7ec40d332e447e82cc503d94eb37
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2024-05-03 23:57:08 +0300

    Update gke.tf

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -14,7 +14,7 @@ variable "gke_num_nodes" {
 }
 # GKE cluster
 resource "google_container_cluster" "primary" {
-  name     = "${var.project_id}-gke"
+  name     = "${var.project_id}-gke-primary"
   location = var.region
   
   # We can't create a cluster with no node pool defined, but we want to only use
@@ -62,5 +62,47 @@ resource "google_container_node_pool" "primary_nodes" {
     ignore_changes = [
       initial_node_count
     ]
+  }
+}
+
+
+# Second GKE cluster
+resource "google_container_cluster" "secondary" {
+  name     = "${var.project_id}-gke-secondary"
+  location = var.region
+  
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+}
+
+# Node pool for the second cluster
+resource "google_container_node_pool" "secondary_nodes" {
+  name       = "${google_container_cluster.secondary.name}-nodes"
+  location   = var.region
+  cluster    = google_container_cluster.secondary.name
+  node_count = var.gke_num_nodes
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      env = var.project_id
+    }
+
+    # preemptible  = true
+    machine_type = "n1-standard-1"
+    tags         = ["gke-node", "${var.project_id}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
   }
 }
