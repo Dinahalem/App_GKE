173078146bda5c74954a021629fb6af1675df055
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2024-05-05 08:06:27 +0300

    Update firewalls.tf

--- terraform/firewalls.tf
+++ terraform/firewalls.tf
@@ -1,13 +1,40 @@
+#Firewall
 #This firewall will allow sshing to the compute instances within VPC.
 
-#resource "google_compute_firewall" "allow-ssh" {
-  #name    = "allow-ssh"
+resource "google_compute_firewall" "allow-ssh" {
+  name    = "allow-ssh"
   #network = google_compute_network.main.name
+  network = "default"
 
-  #allow {
-    #protocol = "tcp"
-    #ports    = ["22"]
-  #}
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
 
-  #source_ranges = ["0.0.0.0/0"]
-#}
+  source_ranges = ["0.0.0.0/0"]
+}
+
+
+resource "google_compute_firewall" "allow-internal" {
+  name    = "allow-internal"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  source_ranges = ["10.0.0.0/8"]
+}
+
+resource "google_compute_firewall" "allow-external" {
+  name    = "allow-external"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
