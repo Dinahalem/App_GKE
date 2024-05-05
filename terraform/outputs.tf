cc77c0d836dd0b6391530f4a60b5b3b2bffaff73
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2024-05-05 07:41:45 +0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,7 +13,19 @@ output "kubernetes_cluster_name" {
   description = "GKE Cluster Name"
 }
 
+
 output "kubernetes_cluster_host" {
   value       = google_container_cluster.primary.endpoint
+  description = "GKE Cluster Host"
+}
+
+
+output "kubernetes_cluster_name" {
+  value       = google_container_cluster.secondary.name
+  description = "GKE Cluster Name"
+}
+
+output "kubernetes_cluster_host" {
+  value       = google_container_cluster.secondary.endpoint
   description = "GKE Cluster Host"
 }
