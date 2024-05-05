cc58f0cfb51bb808d46e156475b9cb82c7759bd4
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2024-05-05 08:10:11 +0300

    Create load balancing.tf

---
+++ terraform/load balancing.tf
@@ -0,0 +1,25 @@
+# lb.tf
+resource "google_compute_global_forwarding_rule" "frontend" {
+  name       = "example-forwarding-rule"
+  target     = google_compute_target_http_proxy.proxy.self_link
+  port_range = "80"
+}
+
+resource "google_compute_target_http_proxy" "proxy" {
+  name    = "example-target-http-proxy"
+  url_map = google_compute_url_map.url_map.self_link
+}
+
+resource "google_compute_url_map" "url_map" {
+  name            = "example-url-map"
+  default_service = google_compute_backend_service.backend_service.self_link
+}
+
+resource "google_compute_backend_service" "backend_service" {
+  name = "example-backend-service"
+  port = "80"
+
+  backend {
+    group = google_container_cluster.primary.instance_group_urls[0]
+  }
+}
