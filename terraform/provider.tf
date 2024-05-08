61fac5ddcabf627bb364e4aa4e36c93271bc0f72
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2024-05-08 05:44:55 +0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,6 +3,11 @@
   #region  = "us-central1"
 #}
 
+# Only needed to enable managed prometheus
+provider "google-beta" {
+  project = local.project_id
+  region  = "us-central1"
+}
 #create bucket to store terraform state
 terraform {
   backend "gcs" {
