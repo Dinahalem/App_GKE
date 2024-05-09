3819bf4adc131ecf28742e8662ebe12e07a929e6
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2024-05-09 08:39:56 +0300

    Update prometheus-ui.tf

--- kubernetes/prometheus-ui.tf
+++ kubernetes/prometheus-ui.tf
@@ -5,11 +5,11 @@ resource "google_service_account" "prometheus_ui" {
 resource "google_service_account_iam_member" "prometheus_ui" {
   service_account_id = google_service_account.prometheus_ui.name
   role               = "roles/iam.workloadIdentityUser"
-  member             = "serviceAccount:${local.project_id}.svc.id.goog[monitoring/prometheus-ui]"
+  member             = "serviceAccount:gke-cicd-422619.svc.id.goog[monitoring/prometheus-ui]"
 }
 
 resource "google_project_iam_member" "prometheus_ui" {
-  project = local.project_id
+  project = "gke-cicd-422619"
   role    = "roles/monitoring.viewer"
   member  = "serviceAccount:${google_service_account.prometheus_ui.email}"
 
